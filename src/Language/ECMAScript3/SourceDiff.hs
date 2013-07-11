c1d83c5e76e8abf3b4e8279836418753376b8edf
Andrey Chudnov <achudnov@stevens.edu>
2013-07-11 18:52:31 -0400

    Switched to the Diff visualization from the Diff package (instead of the homegrown one)

--- src/Language/ECMAScript3/SourceDiff.hs
+++ src/Language/ECMAScript3/SourceDiff.hs
@@ -3,22 +3,13 @@
 module Language.ECMAScript3.SourceDiff where
 
 import Data.Algorithm.Diff
---import Data.Algorithm.DiffOutput
+import Data.Algorithm.DiffOutput
 import Language.ECMAScript3.Syntax
 import Language.ECMAScript3.PrettyPrint
-import Data.List (intersperse)
-
+import Data.List (intersperse, intercalate)
 
 jsDiff :: JavaScript a -> JavaScript a -> String
 jsDiff js1 js2 = 
-  -- let plines = lines . show . prettyPrint
-  -- in  ppDiff $ getGroupedDiff (plines js1) (plines js2)
   let plines = lines . show . prettyPrint
       diff = getGroupedDiff (plines js1) (plines js2)
-      formatDiff :: Diff [String] -> String
-      formatDiff d = let (prefix, strs) = case d of
-                           First ss  -> ('-', ss)
-                           Second ss -> ('+', ss)
-                           Both ss _ -> (' ', ss)
-                     in concat $ intersperse "\n" $  map (prefix:) strs
-  in  concat $ intersperse "\n" $ map formatDiff diff
+  in ppDiff diff
