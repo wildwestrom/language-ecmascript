02be5023df79b5dc02ad27be63b2e69f36c4880c
David Johnson <djohnson.m@gmail.com>
2020-02-13 11:37:22 -0500

    Add overlay.

--- default.nix
+++ default.nix
@@ -1,15 +1,23 @@
 with (builtins.fromJSON (builtins.readFile ./nixpkgs.json));
+let
+  haskellOverlay = self: super: with super.haskell.lib; {
+    cabal2nix = (self.haskellPackages.override {
+      overrides = self: super: {
+        network = dontCheck (super.network);
+        network_3_1_1_1 = dontCheck (super.network_3_1_1_1);
+      };
+    }).cabal2nix;
+  };
+in
 { pkgs ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256;
-  }) {}
+  }) { overlays = [ haskellOverlay ]; }
 , compiler ? "ghc881"
 }:
 let
   overrides = with pkgs.haskell.lib;
     self: super: {
-      network = dontCheck (super.network);
-      network_3_1_1_1 = dontCheck (super.network_3_1_1_1);
       Diff = self.Diff_0_4_0;
     };
   ghc = pkgs.haskell.packages.${compiler}.override { inherit overrides; };
